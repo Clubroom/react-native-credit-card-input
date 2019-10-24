a4c468613e438972dfadd7cc1709719420c77e67
Don Holly <don.holly@gmail.com>
2019-10-24 15:35:49 -0700

    Fix layout for smaller screens

--- src/LiteCreditCardInput.js
+++ src/LiteCreditCardInput.js
@@ -1,18 +1,10 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import {
-  View,
-  Text,
-  StyleSheet,
-  Image,
-  LayoutAnimation,
-  TouchableOpacity,
-  TextInput,
-} from "react-native";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { View, Text, StyleSheet, Image, LayoutAnimation, TouchableOpacity, TextInput } from 'react-native';
 
-import Icons from "./Icons";
-import CCInput from "./CCInput";
-import { InjectedProps } from "./connectToState";
+import Icons from './Icons';
+import CCInput from './CCInput';
+import { InjectedProps } from './connectToState';
 
 const INFINITE_WIDTH = 1000;
 
@@ -20,16 +12,16 @@ const s = StyleSheet.create({
   container: {
     paddingLeft: 10,
     paddingRight: 10,
-    flexDirection: "row",
-    alignItems: "center",
-    overflow: "hidden",
-    backgroundColor: "#ffffff",
+    flexDirection: 'row',
+    alignItems: 'center',
+    overflow: 'hidden',
+    backgroundColor: '#ffffff',
   },
   icon: {
     width: 48,
     height: 40,
-    resizeMode: "contain",
-    backgroundColor: "#ffffff",
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
   },
   expanded: {
     flex: 1,
@@ -38,148 +30,168 @@ const s = StyleSheet.create({
     width: 0,
   },
   leftPart: {
-    overflow: "hidden",
+    overflow: 'hidden',
   },
   rightPart: {
-    overflow: "hidden",
-    flexDirection: "row",
-    backgroundColor: "#ffffff"
+    overflow: 'hidden',
+    flexDirection: 'row',
+    backgroundColor: '#ffffff',
   },
   last4: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: 'center',
   },
   numberInput: {
     width: INFINITE_WIDTH,
   },
   expiryInput: {
-    width: 80,
+    width: '44%',
   },
   cvcInput: {
-    width: 80,
+    width: '33%',
   },
   last4Input: {
-    width: 60,
-    marginLeft: 20,
+    width: '100%',
   },
   input: {
     height: 40,
-    color: "black",
+    color: 'black',
   },
-});
+}); // https://github.com/yannickcr/eslint-plugin-react/issues/106
 
-/* eslint react/prop-types: 0 */ // https://github.com/yannickcr/eslint-plugin-react/issues/106
 export default class LiteCreditCardInput extends Component {
-  static propTypes = {
-    ...InjectedProps,
-
-    placeholders: PropTypes.object,
-
-    inputStyle: Text.propTypes.style,
-
-    validColor: PropTypes.string,
-    invalidColor: PropTypes.string,
-    placeholderColor: PropTypes.string,
-
-    additionalInputsProps: PropTypes.objectOf(PropTypes.shape(TextInput.propTypes)),
-  };
-
-  static defaultProps = {
-    placeholders: {
-      number: "1234 5678 1234 5678",
-      expiry: "MM/YY",
-      cvc: "CVC",
-    },
-    validColor: "",
-    invalidColor: "red",
-    placeholderColor: "gray",
-    additionalInputsProps: {},
-  };
-
   componentDidMount = () => this._focus(this.props.focused);
 
   componentWillReceiveProps = newProps => {
     if (this.props.focused !== newProps.focused) this._focus(newProps.focused);
   };
 
-  _focusNumber = () => this._focus("number");
-  _focusExpiry = () => this._focus("expiry");
+  _focusNumber = () => this._focus('number');
+
+  _focusExpiry = () => this._focus('expiry');
 
   _focus = field => {
     if (!field) return;
     this.refs[field].focus();
     LayoutAnimation.easeInEaseOut();
-  }
+  };
 
   _inputProps = field => {
     const {
-      inputStyle, validColor, invalidColor, placeholderColor,
-      placeholders, values, status,
-      onFocus, onChange, onBecomeEmpty, onBecomeValid,
+      inputStyle,
+      validColor,
+      invalidColor,
+      placeholderColor,
+      placeholders,
+      values,
+      status,
+      onFocus,
+      onChange,
+      onBecomeEmpty,
+      onBecomeValid,
       additionalInputsProps,
     } = this.props;
 
     return {
       inputStyle: [s.input, inputStyle],
-      validColor, invalidColor, placeholderColor,
-      ref: field, field,
+      validColor,
+      invalidColor,
+      placeholderColor,
+      ref: field,
+      field,
 
       placeholder: placeholders[field],
       value: values[field],
       status: status[field],
 
-      onFocus, onChange, onBecomeEmpty, onBecomeValid,
+      onFocus,
+      onChange,
+      onBecomeEmpty,
+      onBecomeValid,
       additionalInputProps: additionalInputsProps[field],
     };
   };
 
   _iconToShow = () => {
-    const { focused, values: { type } } = this.props;
-    if (focused === "cvc" && type === "american-express") return "cvc_amex";
-    if (focused === "cvc") return "cvc";
+    const {
+      focused,
+      values: { type },
+    } = this.props;
+    if (focused === 'cvc' && type === 'american-express') return 'cvc_amex';
+    if (focused === 'cvc') return 'cvc';
     if (type) return type;
-    return "placeholder";
-  }
+    return 'placeholder';
+  };
 
   render() {
-    const { focused, values: { number }, inputStyle, status: { number: numberStatus } } = this.props;
-    const showRightPart = focused && focused !== "number";
+    const {
+      focused,
+      values: { number },
+      inputStyle,
+      status: { number: numberStatus },
+    } = this.props;
+    const showRightPart = focused && focused !== 'number';
 
     return (
       <View style={s.container}>
-        <View style={[
-          s.leftPart,
-          showRightPart ? s.hidden : s.expanded,
-        ]}>
-          <CCInput {...this._inputProps("number")}
-            keyboardType="numeric"
-            containerStyle={s.numberInput} />
+        <View style={[s.leftPart, showRightPart ? s.hidden : s.expanded]}>
+          <CCInput {...this._inputProps('number')} keyboardType="numeric" containerStyle={s.numberInput} />
         </View>
-        <TouchableOpacity onPress={showRightPart ? this._focusNumber : this._focusExpiry }>
+        <TouchableOpacity onPress={showRightPart ? this._focusNumber : this._focusExpiry}>
           <Image style={s.icon} source={Icons[this._iconToShow()]} />
         </TouchableOpacity>
-        <View style={[
-          s.rightPart,
-          showRightPart ? s.expanded : s.hidden,
-        ]}>
-          <TouchableOpacity onPress={this._focusNumber}
-            style={s.last4}>
-            <View pointerEvents={"none"}>
-              <CCInput field="last4"
+        <View style={[s.rightPart, showRightPart ? s.expanded : s.hidden]}>
+          <TouchableOpacity onPress={this._focusNumber} style={s.last4}>
+            <View pointerEvents="none">
+              <CCInput
+                field="last4"
                 keyboardType="numeric"
-                value={ numberStatus === "valid" ? number.substr(number.length - 4, 4) : "" }
+                value={numberStatus === 'valid' ? number.substr(number.length - 4, 4) : ''}
                 inputStyle={[s.input, inputStyle]}
-                containerStyle={[s.last4Input]} />
+                containerStyle={[s.last4Input]}
+              />
             </View>
           </TouchableOpacity>
-          <CCInput {...this._inputProps("expiry")}
+          <CCInput
+            {...this._inputProps('expiry')}
             keyboardType="numeric"
-            containerStyle={s.expiryInput} />
-          <CCInput {...this._inputProps("cvc")}
+            inputStyle={[s.input, { textAlign: 'center' }]}
+            containerStyle={s.expiryInput}
+          />
+          <CCInput
+            {...this._inputProps('cvc')}
             keyboardType="numeric"
-            containerStyle={s.cvcInput} />
+            inputStyle={[s.input, { textAlign: 'center' }]}
+            containerStyle={s.cvcInput}
+          />
         </View>
       </View>
     );
   }
 }
+
+LiteCreditCardInput.propTypes = {
+  ...InjectedProps,
+
+  additionalInputsProps: PropTypes.objectOf(PropTypes.shape(TextInput.propTypes)),
+
+  inputStyle: Text.propTypes.style,
+
+  invalidColor: PropTypes.string,
+  placeholderColor: PropTypes.string,
+  placeholders: PropTypes.object,
+
+  validColor: PropTypes.string,
+};
+
+LiteCreditCardInput.defaultProps = {
+  placeholders: {
+    number: '1234 5678 1234 5678',
+    expiry: 'MM/YY',
+    cvc: 'CVC',
+  },
+  validColor: '',
+  invalidColor: 'red',
+  placeholderColor: 'gray',
+  additionalInputsProps: {},
+};
